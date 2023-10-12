62bfa2f9063ecaaa04eb7d210134d2f3196b6ab4
Nam G VU <namgivu@gmail.com>
2023-10-12 22:46:39 +0700

    fix tc @ missing comma

--- s00_bailam.py
+++ s00_bailam.py
@@ -25,7 +25,7 @@ hi(None)                     | Hi!
 ------------------- mucdo: kho -----------------
 hi('Mom', 'Dad')             | Hi Mom, and Dad!
 hi('A', 'B', 'C')            | Hi A, B, and C!
-hi('1', '22', '333', '4444') | Hi 1, 22, 333 and 4444!
+hi('1', '22', '333', '4444') | Hi 1, 22, 333, and 4444!
 """
 #endregion debai
 
